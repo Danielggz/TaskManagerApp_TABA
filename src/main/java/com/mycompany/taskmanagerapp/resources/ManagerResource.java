0c0cccc2bd187bc79daa2cb8b3ad14bd985151ab
Danielggz <dggz1993@gmail.com>
2024-12-04 10:40:22 +0000

    Rearrange files in folders

--- src/main/java/com/mycompany/taskmanagerapp/resources/ManagerResource.java
+++ src/main/java/com/mycompany/taskmanagerapp/resources/ManagerResource.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/GenericResource.java to edit this template
  */
-package com.mycompany.taskmanagerapp;
+package com.mycompany.taskmanagerapp.resources;
 
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.UriInfo;
