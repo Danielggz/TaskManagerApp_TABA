5e9dd06fd61f947fc3c6e40a3a77dde7a09254a6
Danielggz <dggz1993@gmail.com>
2024-12-11 10:13:04 +0000

    Services and database basic classes

---
+++ src/main/java/com/mycompany/taskmanagerapp/services/TaskService.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.taskmanagerapp.services;
+
+/**
+ * @author Daniel García
+ * 11/12/2024
+ */
+public class TaskService {
+
+}
