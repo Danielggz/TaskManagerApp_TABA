d51114d7ba275a08920a916d7776245861d03097
Danielggz <dggz1993@gmail.com>
2024-12-11 22:32:43 +0000

    Task Service and Task Resource endpoints

--- src/main/java/com/mycompany/taskmanagerapp/resources/TaskResource.java
+++ src/main/java/com/mycompany/taskmanagerapp/resources/TaskResource.java
@@ -4,6 +4,9 @@
  */
 package com.mycompany.taskmanagerapp.resources;
 
+import com.mycompany.taskmanagerapp.models.Task;
+import com.mycompany.taskmanagerapp.models.Task;
+import com.mycompany.taskmanagerapp.services.TaskService;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.Produces;
@@ -12,6 +15,10 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PUT;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
 
 /**
  * REST Web Service
@@ -21,7 +28,8 @@ import jakarta.enterprise.context.RequestScoped;
 @Path("tasks")
 @RequestScoped
 public class TaskResource {
-
+    
+    TaskService taskService = new TaskService();
     /**
      * Creates a new instance of TasksResource
      */
@@ -29,16 +37,51 @@ public class TaskResource {
     }
 
     //Create task
-    
+    @POST
+    @Path("/newTask")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Task createTask(Task projPost) {
+        return taskService.createTask(projPost);
+    }
     //Retrieve all tasks (XML)
-    
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Task> getTasksXML() {
+        return taskService.getAllTasks();
+    }
     //Retrieve all tasks (JSON)
-    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Task> getTasksJSON() {
+        return taskService.getAllTasks();
+    }
     //Retrieve task (XML)
-    
+    @GET
+    @Path("/{projectId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Task getTaskXML(@PathParam("projId") int projId) {
+        return taskService.getTask(projId);
+    }
     //Retrieve task (JSON)
-    
+    @GET
+    @Path("/{projectId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Task getTaskJSON(@PathParam("projId") int projId) {
+        return taskService.getTask(projId);
+    }
     //Update task
-    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/updateProj/{projectId}")
+    public Task updateTask(Task updtProj) {
+	return taskService.updateTask(updtProj);
+    }
     //Delete task
+    @GET
+    @Path("/deleteProj/{projectId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Task deleteTask(int idProj){
+        return taskService.deleteTask(idProj);
+    }
 }
--- src/main/java/com/mycompany/taskmanagerapp/services/TaskService.java
+++ src/main/java/com/mycompany/taskmanagerapp/services/TaskService.java
@@ -5,10 +5,36 @@
 
 package com.mycompany.taskmanagerapp.services;
 
+import com.mycompany.taskmanagerapp.databases.Database;
+import com.mycompany.taskmanagerapp.models.Task;
+import java.util.List;
+
 /**
  * @author Daniel García
  * 11/12/2024
  */
 public class TaskService {
-
+    Database db = new Database();
+    private List<Task> taskList = db.getTasks();
+    
+    public Task createTask(Task newT){
+        taskList.add(newT);
+        return newT;
+    }
+    
+    public List<Task> getAllTasks(){
+        return taskList;
+    }
+    
+    public Task getTask(int idTask){
+        return taskList.get(idTask);
+    }
+    
+    public Task updateTask(Task t){
+        return taskList.set(t.getId(), t);
+    }
+    
+    public Task deleteTask(int idTask){
+        return taskList.remove(idTask);
+    }
 }
